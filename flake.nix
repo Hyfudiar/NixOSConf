f3326ba7e09890c420ab681a1aa92e1895d73123
root <hyfudiarmusic@gmail.com>
2025-11-15 23:23:42 -0500

    resetting

--- flake.nix
+++ flake.nix
@@ -1,19 +1,12 @@
 {
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
-    musnix  = { url = "github:musnix/musnix"; };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
-  outputs = inputs: rec {
-    nixosConfigurations = {
-      example-config = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules =
-          [ # ...
-            inputs.musnix.nixosModules.musnix
-            ./configuration.nix
-          ];
-        specialArgs = { inherit inputs; };
-      };
+  outputs = { self, nixpkgs }: {
+    # replace 'joes-desktop' with your hostname here.
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
     };
   };
 }
