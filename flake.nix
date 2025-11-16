8a5e591b409bb60587269a627a998be0e605351e
root <hyfudiarmusic@gmail.com>
2025-11-15 22:51:36 -0500

    switched to musnix version

--- flake.nix
+++ flake.nix
@@ -1,62 +1,19 @@
 {
-  description = "A very basic flake with Musnix support";
-
   inputs = {
-    #   nixpkgs.url = "github:NixOS/nixpkgs/<branch name>";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05-small";
-
-  
-    # nix-snapd.url = "github:nix-community/nix-snapd";
-    # nix-snapd.inputs.nixpkgs.follows = "nixpkgs";
-
-    musnix.url  = "github:musnix/musnix";
-    # home-manager = {
-    #  url = "github:nix-community/home-manager/release-24.11";
-    #  # url = "github:nix-community/home-manager/release-unstable";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+    musnix  = { url = "github:musnix/musnix"; };
   };
-
-  outputs = { self, nixpkgs, musnix, home-manager, ... } @ inputs: 
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [ musnix.overlay ];
-    };
-  in
-  rec {
-    packages.${system} = {
-      hello = pkgs.hello;
-      default = pkgs.hello;
-    };
-
+  outputs = inputs: rec {
     nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          musnix.nixosModules.musnix
-          ./configuration.nix
-          # home-manager.nixosModules.home-manager
-          # {
-          #  home-manager.useGlobalPkgs = true;
-          #  home-manager.useUserPackages = true;
-          #  home-manager.users.steve = import ./home.nix;
-          # }
-          # nix-snapd.nixosModules.default
-          # {
-          #   services.snap.enable = true;
-          # }
-        ];
+      example-config = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules =
+          [ # ...
+            inputs.musnix.nixosModules.musnix
+            ./configuration.nix
+          ];
         specialArgs = { inherit inputs; };
       };
     };
   };
-
-
-
-
-
-}
+# ...
