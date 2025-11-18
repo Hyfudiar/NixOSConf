a1f05c32219f003c4b6b1a5bbcb7ad4156a61191
root <hyfudiarmusic@gmail.com>
2025-11-18 17:49:30 -0500

    going with flake, added musnix and nixos-hardware

--- flake.nix
+++ flake.nix
@@ -1,12 +1,32 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+    musnix  = { url = "github:musnix/musnix"; };
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = { self, nixpkgs }: {
-    # replace 'joes-desktop' with your hostname here.
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+  outputs = inputs: rec {
+    nixosConfigurations.bepithonk = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules =
+          [ # ...
+            inputs.musnix.nixosModules.musnix
+            ./configuration.nix
+            inputs.nixos-hardware.nixosModules.lenovo-thinkpad-p43s
+          ];
+        specialArgs = { inherit inputs; };
+      };
     };
   };
 }
+#{
+#  inputs = {
+#    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+#  };
+#  outputs = { self, nixpkgs }: {
+#    # replace 'joes-desktop' with your hostname here.
+#    nixosConfigurations.bepithonk = nixpkgs.lib.nixosSystem {
+#      system = "x86_64-linux";
+#      modules = [ ./configuration.nix ];
+#    };
+#  };
+#}
