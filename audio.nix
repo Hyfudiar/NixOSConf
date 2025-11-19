f9da55099297ac1332663213499bc2f2c6390d7c
root <hyfudiarmusic@gmail.com>
2025-11-19 09:56:12 -0500

    took out extra line in audio

--- audio.nix
+++ audio.nix
@@ -4,7 +4,6 @@
 
 {
 
-  environment.systemPackages = with pkgs; [ libjack2 jack2 qjackctl ];
   environment.systemPackages = with pkgs; [ pavucontrol libjack2 jack2 qjackctl jack2Full jack_capture ];
   security.sudo.extraConfig = ''
     moritz  ALL=(ALL) NOPASSWD: ${pkgs.systemd}/bin/systemctl
