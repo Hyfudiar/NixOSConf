b46e07cdf07f191eecbcc0c16370b75979e93e2c
hyfudiar <hyfudiarmusic@gmail.com>
2025-12-21 00:32:26 -0500

    hopefully installing homemanager v2

--- configuration.nix
+++ configuration.nix
@@ -101,9 +101,9 @@
     ];
   };
   
-  home-manager.users.eve = { pkgs, ... }: {
-    home.packages = [ pkgs.cowsay ];
-    programs.bash.enable = true;
+  #home-manager.users.eve = { pkgs, ... }: {
+  #  home.packages = [ pkgs.cowsay ];
+  #  programs.bash.enable = true;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -112,9 +112,9 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-    home.stateVersion = "25.11"; # Please read the comment before changing. 
+  #  home.stateVersion = "25.11"; # Please read the comment before changing. 
 
-  };
+  #};
 
   # Install firefox.
   programs.firefox.enable = true;
