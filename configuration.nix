55074f89852dad23f54e222c57e8a6dab7174c18
hyfudiar <hyfudiarmusic@gmail.com>
2026-01-04 18:54:11 -0500

    got rid of vars

--- configuration.nix
+++ configuration.nix
@@ -202,108 +202,11 @@
 
   # -hyfu- for running Godot binaries
   
-  programs.nix-ld = {
-    enable = true;
-  };
-
-  environment.variables = {
-      NIX_LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
-        stdenv.cc.cc
-        openssl
-        xorg.libXcomposite
-        xorg.libXtst
-        xorg.libXrandr
-        xorg.libXext
-        xorg.libX11
-        xorg.libXfixes
-        libGL
-        libva
-        pipewire
-        xorg.libxcb
-        xorg.libXdamage
-        xorg.libxshmfence
-        xorg.libXxf86vm
-        libelf
-        
-        # Required
-        glib
-        gtk2
-        bzip2
-        
-        # Without these it silently fails
-        xorg.libXinerama
-        xorg.libXcursor
-        xorg.libXrender
-        xorg.libXScrnSaver
-        xorg.libXi
-        xorg.libSM
-        xorg.libICE
-        gnome2.GConf
-        nspr
-        nss
-        cups
-        libcap
-        SDL2
-        libusb1
-        dbus-glib
-        ffmpeg
-        # Only libraries are needed from those two
-        libudev0-shim
-        
-        # Verified games requirements
-        xorg.libXt
-        xorg.libXmu
-        libogg
-        libvorbis
-        SDL
-        SDL2_image
-        glew110
-        libidn
-        tbb
-        
-        # Other things from runtime
-        flac
-        freeglut
-        libjpeg
-        libpng
-        libpng12
-        libsamplerate
-        libmikmod
-        libtheora
-        libtiff
-        pixman
-        speex
-        SDL_image
-        SDL_ttf
-        SDL_mixer
-        SDL2_ttf
-        SDL2_mixer
-        libappindicator-gtk2
-        libdbusmenu-gtk2
-        libindicator-gtk2
-        libcaca
-        libcanberra
-        libgcrypt
-        libvpx
-        librsvg
-        xorg.libXft
-        libvdpau
-        pango
-        cairo
-        atk
-        gdk-pixbuf
-        fontconfig
-        freetype
-        dbus
-        alsa-lib
-        expat
-        # Needed for electron
-        libdrm
-        mesa
-        libxkbcommon
-      ];
-      NIX_LD = lib.mkForce lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
-  };  
+  programs.nix-ld.enable = true;
+  ## If needed, you can add missing libraries here. nix-index-database is your friend to
+  ## find the name of the package from the error message:
+  ## https://github.com/nix-community/nix-index-database
+  programs.nix-ld.libraries = options.programs.nix-ld.libraries.default ++ (with pkgs; [ ]); 
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
