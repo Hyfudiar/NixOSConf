0ef483299df7fb089e7997f2775d1d4312a1e6b8
hyfudiar <hyfudiarmusic@gmail.com>
2026-06-29 11:09:37 -0400

    another attempt

--- configuration.nix
+++ configuration.nix
@@ -54,7 +54,6 @@
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
-  #services.xserver.videoDrivers = [ "intel" ];
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
