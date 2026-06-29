40b72ccf139b1897d246d0a28933104e5727581c
hyfudiar <hyfudiarmusic@gmail.com>
2026-06-28 22:43:31 -0400

    specify xorg intel driver

--- configuration.nix
+++ configuration.nix
@@ -54,6 +54,7 @@
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "intel" ];
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
