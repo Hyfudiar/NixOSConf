142037d228cc627e03f1e54967b3a208510aacad
hyfudiar <hyfudiarmusic@gmail.com>
2026-06-29 11:28:13 -0400

    what a pain

--- nvidia.nix
+++ nvidia.nix
@@ -28,8 +28,10 @@
   };
 
   hardware.nvidia.prime = {
-    sync.enable = false;
-
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:60:0:0";
   };
